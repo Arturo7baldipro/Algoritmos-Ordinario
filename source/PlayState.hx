d87494351cd3aa78e551e2d0e886185f820ae1e8
Arturo7baldipro <153790614+Arturo7baldipro@users.noreply.github.com>
2024-05-27 09:14:11 -0600

    Prueba Inicial
    -Agregacion de una BusquedaLineal
    -Clase Personaje agregada (sprites incluidos con opcion de elegir cual agregar con sus componentes)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,20 +1,70 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var nombre:FlxText;
+	var sombrero:FlxText;
+	var acesorio:FlxText;
+	var personajes = new FlxTypedGroup<FlxSprite>();
+	var etePersonajes:Array<String> = ['Pepe', 'Tematch', "Random1", "Random2", "Random3"];
+
 	override public function create()
 	{
 		super.create();
-		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		/*
+				personaje = new Personaje(500, 100, "Tematch");
+				add(personaje);
+			 
 
-		text.screenCenter();
-		add(text);
+
+			acesorio = new FlxText(sombrero.x + 200);
+			acesorio.text = personaje.curAccesorio;
+			acesorio.antialiasing = false;
+			add(acesorio);
+		 */
+
+		personajes = new FlxTypedGroup<FlxSprite>();
+		add(personajes);
+
+		for (i in 0...etePersonajes.length)
+		{
+			var personaje:FlxSprite = new FlxSprite();
+			personaje = new Personaje(100, 0, etePersonajes[i]);
+			personaje.ID = i;
+			personaje.y = 60 + (i * 100);
+			personajes.add(personaje);
+		}
+		var prueba = etePersonajes[1];
+
+		var prueba = FlxG.random.int(0, etePersonajes.length);
+
+		linealSearch(etePersonajes, "Tematch");
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+
+	// Funciona
+	public function linealSearch(array:Array<String>, search:String)
+	{
+		for (i in 0...array.length)
+		{
+			if (array.contains(search))
+			{
+				FlxG.camera.flash(FlxColor.RED, 1);
+			}
+			FlxG.camera.flash(FlxColor.BLUE, 1);
+		}
 	}
 }
