e4300a4a208a1583846487c5815829df80036f6c
Arturo7baldipro <153790614+Arturo7baldipro@users.noreply.github.com>
2024-05-28 09:04:49 -0600

    Caracteristicas
    -Agregadas las 10 caracteristicas minimas

--- source/Personaje.hx
+++ source/Personaje.hx
@@ -7,24 +7,34 @@ import flixel.text.FlxText;
 class Personaje extends FlxSprite
 {
 	public var curCharacter:String;
+
+	public var curEdad:String;
+	public var curColorOjos:String;
+	public var curRopa:String;
+	public var curLunar:String;
+	public var curAnteojos:String;
 	public var curSombrero:String;
 	public var curAccesorio:String;
 	public var curPelo:String;
 	public var curPiel:String;
 	public var curSexo:String;
 
-	public function new(x:Float = 0, y:Float = 0, character:String = "Pepe", sombrero:String = "Ninguno", accesorio:String = "Ninguno",
-			pelo:String = "Ninguno", piel:String = "Ninguno", sexo:String = "Ninguno")
+	public function new(x:Float = 0, y:Float = 0, character:String = "Pepe", edad:String = "Ninguno", colorOjos:String = "Ninguno", ropa:String = "Ninguno",
+			lunar:String = "Ninguno", anteojos:String = "Ninguno", sombrero:String = "Ninguno", accesorio:String = "Ninguno", pelo:String = "Ninguno",
+			piel:String = "Ninguno", sexo:String = "Ninguno")
 	{
 		super(x, y);
-		this.curCharacter = character;
+		this.curCharacter = character; // No cuenta como caracteristica
+		this.curColorOjos = colorOjos;
+		this.curRopa = ropa;
+		this.curLunar = lunar;
+		this.curAnteojos = anteojos;
 		this.curSombrero = sombrero;
 		this.curAccesorio = accesorio;
 		this.curPelo = pelo;
 		this.curPiel = piel;
 		this.curSexo = sexo;
-
-		// Usen los "cur" si quieren inicializar variables de un personajes
+		// Usen los "cur" si quieren cambiar variables de un personajes, tienen de default "Ninguno";
 		switch (curCharacter)
 		{
 			case "Pepe":
