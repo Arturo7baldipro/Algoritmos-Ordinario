2a609aece723e327c90b8174e56cb6b4f6a36ed3
Touch89 <142188965+Touch89@users.noreply.github.com>
2024-06-02 22:06:39 -0600

    Update Personaje.hx

--- source/Personaje.hx
+++ source/Personaje.hx
@@ -8,54 +8,35 @@ import haxe.macro.Expr.Case;
 class Personaje extends FlxSprite
 {
 	public var curCharacter:String;
-
+	public var curRaza:String;
+	public var curBelloFacial:String;
 	public var curEdad:String;
-	public var curColorOjos:String;
 	public var curRopa:String;
 	public var curLunar:String;
-	public var curAnteojos:String;
 	public var curSombrero:String;
-	public var curAccesorio:String;
 	public var curPelo:String;
 	public var curPiel:String;
 	public var curSexo:String;
 	public var curLentes:String;
 
-	public function new(x:Float = 0, y:Float = 0, character:String = "Pepe", edad:String = "Ninguno", colorOjos:String = "Ninguno", ropa:String = "Ninguno",
-			lunar:String = "Ninguno", anteojos:String = "Ninguno", sombrero:String = "Ninguno", accesorio:String = "Ninguno", pelo:String = "Ninguno",
-			piel:String = "Ninguno", sexo:String = "Ninguno", lentes:String = "Ninguno")
+	public function new(x:Float = 0, y:Float = 0, character:String = "Pepe", edad:String = "Ninguno", ropa:String = "Ninguno", anteojos:String = "Ninguno",
+			sombrero:String = "Ninguno", pelo:String = "Ninguno", belloFacial:String = "Ninguno", raza:String = "Ninguno", piel:String = "Ninguno",
+			sexo:String = "Ninguno", lentes:String = "Ninguno")
 	{
 		super(x, y);
 		this.curCharacter = character; // No cuenta como caracteristica
-		this.curColorOjos = colorOjos;
 		this.curRopa = ropa;
-		this.curLunar = lunar;
-		this.curAnteojos = anteojos;
 		this.curSombrero = sombrero;
-		this.curAccesorio = accesorio;
 		this.curPelo = pelo;
 		this.curPiel = piel;
 		this.curSexo = sexo;
+		this.curEdad = edad;
+		this.curBelloFacial = belloFacial;
+		this.curRaza = raza;
 		this.curLentes = lentes;
 		// Usen los "cur" si quieren cambiar variables de un personajes, tienen de default "Ninguno";
 		switch (curCharacter)
 		{
-			/*case "Pepe":
-					loadGraphic("assets/images/PlaceHolder.png");
-					antialiasing = false;
-					curSombrero = "PlaceHolder";
-					curAccesorio = "Ninguno";
-					curPelo = "Afro";
-					curPiel = "Cafe";
-					curSexo = "Hombre";
-				case "Sech":
-					loadGraphic("assets/images/PlaceHolder2.png");
-					antialiasing = false;
-					curSombrero = "Sech";
-					curAccesorio = "bola";
-					curPelo = "Esponjado";
-					curPiel = "Cafe";
-					curSexo = "Hombre"; */
 			case "Juan":
 				loadGraphic("assets/images/Juan.png");
 				antialiasing = false;
