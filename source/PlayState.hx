3e1900d039a30a3e9231749a0d04754b4c677958
Arturo7baldipro <153790614+Arturo7baldipro@users.noreply.github.com>
2024-06-04 21:24:57 -0600

    Terror miedo (emilio edition)
    https://www.youtube.com/watch?v=d3Dtqq1uGjg

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -244,6 +244,204 @@ class PlayState extends FlxState
 		inputTXT.text = "Introducir Personaje";
 		inputTXT.size = 10;
 		add(inputTXT);
+
+		var randomizer = new Randomizer();
+
+		while (randomizer.numeros.length > 0)
+		{
+			var numeroAleatorio = randomizer.obtenerNumeroAleatorio();
+			trace(numeroAleatorio); // Imprime el número aleatorio
+			switch (numeroAleatorio)
+			{
+				// 21 opciones
+				case 1: // blusa
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 2 || spr.ID == 15 || spr.ID == 16 || spr.ID == 20 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 2: // traje
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 1 || spr.ID == 6 || spr.ID == 8 || spr.ID == 9 || spr.ID == 11 || spr.ID == 14 || spr.ID == 22)
+						{
+							spr.kill();
+						}
+					});
+				case 3: // playera
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 4 || spr.ID == 7 || spr.ID == 10 || spr.ID == 12 || spr.ID == 13 || spr.ID == 17 || spr.ID == 18)
+						{
+							spr.kill();
+						}
+					});
+				case 4: // sueter
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 3 || spr.ID == 5 || spr.ID == 19 || spr.ID == 21 || spr.ID == 23)
+						{
+							spr.kill();
+						}
+					});
+				case 5: // tiene sombrero
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 18 || spr.ID == 19)
+						{
+							spr.kill();
+						}
+					});
+				case 6: // pelo negro
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 1 || spr.ID == 5 || spr.ID == 7 || spr.ID == 10 || spr.ID == 11 || spr.ID == 12 || spr.ID == 13
+							|| spr.ID == 14 || spr.ID == 23)
+						{
+							spr.kill();
+						}
+					});
+				case 7: // pelo cafe
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 2 || spr.ID == 3 || spr.ID == 4 || spr.ID == 16 || spr.ID == 18 || spr.ID == 19 || spr.ID == 20 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 8: // pelo wero
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 15)
+						{
+							spr.kill();
+						}
+					});
+				case 9: // pelo pelirrojo
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 22)
+						{
+							spr.kill();
+						}
+					});
+				case 10: // piel oscura
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 2)
+						{
+							spr.kill();
+						}
+					});
+				case 11: // piel cafe
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 23 || spr.ID == 16)
+						{
+							spr.kill();
+						}
+					});
+				case 12: // piel clara
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 1 || spr.ID == 3 || spr.ID == 4 || spr.ID == 5 || spr.ID == 6 || spr.ID == 7 || spr.ID == 8 || spr.ID == 9
+							|| spr.ID == 10 || spr.ID == 11 || spr.ID == 12 || spr.ID == 13 || spr.ID == 14 || spr.ID == 15 || spr.ID == 17 || spr.ID == 18
+							|| spr.ID == 19 || spr.ID == 20 || spr.ID == 21 || spr.ID == 22 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 13: // hombre
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 1 || spr.ID == 2 || spr.ID == 3 || spr.ID == 5 || spr.ID == 6 || spr.ID == 8 || spr.ID == 9
+							|| spr.ID == 10 || spr.ID == 11 || spr.ID == 12 || spr.ID == 13 || spr.ID == 14 || spr.ID == 15 || spr.ID == 17 || spr.ID == 18
+							|| spr.ID == 19 || spr.ID == 23)
+						{
+							spr.kill();
+						}
+					});
+				case 14: // mujer
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 4 || spr.ID == 7 || spr.ID == 13 || spr.ID == 14 || spr.ID == 16 || spr.ID == 20 || spr.ID == 22 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 15: // joven
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 1 || spr.ID == 2 || spr.ID == 3 || spr.ID == 4 || spr.ID == 5 || spr.ID == 6 || spr.ID == 7
+							|| spr.ID == 10 || spr.ID == 11 || spr.ID == 12 || spr.ID == 14 || spr.ID == 15 || spr.ID == 16 || spr.ID == 18 || spr.ID == 19
+							|| spr.ID == 20 || spr.ID == 22 || spr.ID == 23 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 16: // mayor de edad
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 21 || spr.ID == 17 || spr.ID == 13 || spr.ID == 9 || spr.ID == 8)
+						{
+							spr.kill();
+						}
+					});
+				case 17: // bello facial
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 5 || spr.ID == 6 || spr.ID == 9 || spr.ID == 18 || spr.ID == 21 || spr.ID == 23)
+						{
+							spr.kill();
+						}
+					});
+				case 18: // afroamericano
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 0 || spr.ID == 16 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 19: // caucasico
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 1 || spr.ID == 2 || spr.ID == 3 || spr.ID == 4 || spr.ID == 5 || spr.ID == 6 || spr.ID == 7 || spr.ID == 8
+							|| spr.ID == 9 || spr.ID == 12 || spr.ID == 13 || spr.ID == 14 || spr.ID == 15 || spr.ID == 18 || spr.ID == 19 || spr.ID == 20
+							|| spr.ID == 21 || spr.ID == 22 || spr.ID == 23)
+						{
+							spr.kill();
+						}
+					});
+				case 20: // asiatico
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 17 || spr.ID == 11 || spr.ID == 10)
+						{
+							spr.kill();
+						}
+					});
+				case 21: // usa lentes
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 1 || spr.ID == 7 || spr.ID == 8 || spr.ID == 12 || spr.ID == 13 || spr.ID == 16 || spr.ID == 17 || spr.ID == 19
+							|| spr.ID == 21 || spr.ID == 23 || spr.ID == 24)
+						{
+							spr.kill();
+						}
+					});
+				case 22: // pelo blanco
+					personajesBot.forEach(function(spr:Personaje)
+					{
+						if (spr.ID == 8 || spr.ID == 9 || spr.ID == 17 || spr.ID == 21)
+						{
+							spr.kill();
+						}
+					});
+			}
+		}
 	}
 
 	override public function update(elapsed:Float)
@@ -627,5 +825,38 @@ class PlayState extends FlxState
 	public function lentesFunction_SI()
 	{
 		FlxG.camera.flash(FlxColor.RED, 1);
+	}
+}
+
+class Randomizer
+{
+	public var numeros:Array<Int>;
+
+	public function new()
+	{
+		// Inicializa el array con los números del 1 al 21
+		numeros = new Array<Int>();
+		for (i in 1...22)
+		{
+			numeros.push(i);
+		}
+	}
+
+	public function obtenerNumeroAleatorio():Int
+	{
+		// Comprueba si el array está vacío
+		if (numeros.length == 0)
+		{
+			throw "No quedan números";
+		}
+
+		// Genera un índice aleatorio
+		var indiceAleatorio = Math.floor(Math.random() * numeros.length);
+
+		// Obtiene y elimina el número en el índice aleatorio
+		var numero = numeros[indiceAleatorio];
+		numeros.splice(indiceAleatorio, 1);
+
+		return numero;
 	}
 }
